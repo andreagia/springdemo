810a603f4183b835a152f77202199bb7c404e261
andreagia <andreagia71@gmail.com>
2018-10-16 18:15:29 +0200

    Aggiunto la gestione dell'autorizzazioni dell'utenye ADMIN e USER possono accedere a parti diverse

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
 INSERT INTO app_role (id, role_name, description) VALUES (1, 'ROLE_USER', 'Standard User - Has no admin rights');
 INSERT INTO app_role (id, role_name, description) VALUES (2, 'ROLE_ADMIN', 'Admin User - Has permission to perform admin tasks');
-
 -- USER
 -- non-encrypted password: jwtpass
 INSERT INTO app_user (id, first_name, last_name, password, username, active) VALUES (1, 'John', 'Doe', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'john.doe', true);
