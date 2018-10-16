1aedced6f342ad2bb48040131373120ea090d0a3
andreagia <andreagia71@gmail.com>
2018-10-16 18:31:19 +0200

    Aggiunto database JPA al login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,7 +6,6 @@ INSERT INTO app_role (id, role_name, description) VALUES (2, 'ROLE_ADMIN', 'Admi
 INSERT INTO app_user (id, first_name, last_name, password, username, active) VALUES (1, 'John', 'Doe', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'john.doe', true);
 INSERT INTO app_user (id, first_name, last_name, password, username, active) VALUES (2, 'Admin', 'Admin', '$2a$10$qtH0F1m488673KwgAfFXEOWxsoZSeHqqlB/8BTt3a6gsI5c2mdlfe', 'admin.admin', true);
 
-
 INSERT INTO user_role(user_id, role_id) VALUES(1,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,1);
 INSERT INTO user_role(user_id, role_id) VALUES(2,2);
